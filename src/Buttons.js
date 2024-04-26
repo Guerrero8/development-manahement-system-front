6ea5592fa392c43196dc128a1a20634dfa671c5a
Guerrero8 <Igo2rlol@gmail.com>
2024-04-26 17:21:59 +0300

    create ModalWindow

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,11 +1,25 @@
-// Buttons.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Buttons.css';
+import CreateModalWindow from './CreateModalWindow'; // Подключаем компонент с всплывающим окном
 
 function Buttons() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    // Функция для открытия всплывающего окна
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    // Функция для закрытия всплывающего окна
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className="buttons">
-            <button onClick={() => console.log('Создать')}>Добавить</button>
+            <button onClick={openModal}>Добавить</button>
+            {/* Условный рендеринг всплывающего окна */}
+            {isModalOpen && <CreateModalWindow onClose={closeModal} />}
             <button onClick={() => console.log('Удалить')}>Удалить</button>
             <button onClick={() => console.log('Обновить')}>Обновить</button>
             <button className="search-button" onClick={() => console.log('Создать')}>Поиск</button>
--- src/CreateModalWindow.js
+++ src/CreateModalWindow.js
@@ -1,11 +1,11 @@
-// CreateModalWindow.js
 import React from 'react';
+import './CreateModalWindow.css';
 
 function CreateModalWindow({ onClose }) {
     return (
         <div className="modal">
             <div className="modal-content">
-                <button onClick={onClose}>Закрыть</button>
+                <button className="close-button" onClick={onClose}>✖</button>
                 <button>Заказ</button>
                 <button>Клиент</button>
                 <button>ФНС клиента</button>
